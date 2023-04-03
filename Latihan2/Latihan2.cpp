3be139507ff3107580fb9b992f5fdd1219750b72
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-04-03 11:24:38 +0700

    deklarasikan variabel

--- Latihan2/Latihan2.cpp
+++ Latihan2/Latihan2.cpp
@@ -14,3 +14,24 @@ struct mahasiswa		//membuat struktur variabel mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+int main() {
+	mahasiswa mhs;			//deklarasikan variabel
+
+	cout << "Masukkan NIM = ";
+	cin >> mhs.NIM;
+	cout << "Masukkan Nama = ";
+	cin >> mhs.nama;
+	cout << "Alamat = ";
+	cout << "\n\tMasukkan Desa = ";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukkan Kota = ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukkan Umur = ";
+	cin >> mhs.umur;
+
+	cout << "\n\nNIM = " << mhs.NIM;
+	cout << "\nNama = " << mhs.nama;
+	cout << "\nDesa = " << mhs.alamat.desa;
+	cout << "\nKota = " << mhs.alamat.kota;
+	cout << "\nUmur = " << mhs.umur;
+}
