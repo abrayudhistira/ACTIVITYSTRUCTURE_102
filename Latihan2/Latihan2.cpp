cbc65661ecc926aa912361f6b17250e974ce1907
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-04-03 11:21:43 +0700

    membuat struktur variabel mahasiswa

--- Latihan2/Latihan2.cpp
+++ Latihan2/Latihan2.cpp
@@ -6,3 +6,11 @@ struct AlamatDetail			//membuat struktur variabel AlamatDetail
 	string desa;
 	string kota;
 };
+
+struct mahasiswa		//membuat struktur variabel mahasiswa
+{
+	string NIM;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
