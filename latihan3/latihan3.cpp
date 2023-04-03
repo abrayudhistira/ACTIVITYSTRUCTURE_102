79d544f34d3bf9322256d2bb69b1dd585676148f
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-04-03 11:40:40 +0700

    latihan 3

--- latihan3/latihan3.cpp
+++ latihan3/latihan3.cpp
@@ -10,7 +10,7 @@ struct AlamatDetail			//membuat struktur variabel AlamatDetail dengan karakter
 struct Mahasiswa		//membuat struktur variabel mahasiswa dengan karakter
 {
 	char NIM[20];
-	char nama[20];
+	char Nama[20];
 	AlamatDetail alamat;
 	int umur;
 };
@@ -18,5 +18,28 @@ struct Mahasiswa		//membuat struktur variabel mahasiswa dengan karakter
 int main() {
 	Mahasiswa mhs[3];			// mendeklarasikan variabel mahasiswa
 
-	for
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukkan NIM = ";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "Masukkan Nama = ";
+		cin.getline(mhs[i].Nama, 25);
+		cout << "Alamat = ";
+		cout << "\n\tMasukkan Desa = ";
+		cin.getline(mhs[i].alamat.Desa, 20);
+		cout << "\n\tMasukkan Kota = ";
+		cin.getline(mhs[i].alamat.Kota, 20);
+		cout << "Masukkan Umur = ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\n\nNIM = " << mhs[i].NIM;
+		cout << "\nNama = " << mhs[i].Nama;
+		cout << "\nDesa = " << mhs[i].alamat.Desa;
+		cout << "\nKota = " << mhs[i].alamat.Kota;
+		cout << "\nUmur = " << mhs[i].umur;
+	}
 }
