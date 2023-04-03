1d6f521f309f5e334bdf7aaef3cfb0c488704477
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-04-03 11:28:10 +0700

    membuat struktur variabel mahasiswa dengan karakter

--- latihan3/latihan3.cpp
+++ latihan3/latihan3.cpp
@@ -6,3 +6,11 @@ struct AlamatDetail			//membuat struktur variabel AlamatDetail dengan karakter
 	char Desa[20];
 	char Kota[20];
 };
+
+struct mahasiswa		//membuat struktur variabel mahasiswa dengan karakter
+{
+	char NIM[20];
+	char nama[20];
+	AlamatDetail alamat;
+	int umur;
+};
